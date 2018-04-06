0aa4935c52c5d1b9dd107738a31ce835a50a44bd
Rahul <roballjobs@gmail.com>
2018-04-06 17:11:41 -0400

    Rating feedback widget

---
+++ react/rating/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Rating from './Rating';
+
+ReactDOM.render(<Rating size='5' selectedRating='0'/>, document.getElementById('root'));
