ee250e82c835888415d7fd7dbb6fa4fd39d56aea
Rahul <roballjobs@gmail.com>
2019-10-06 12:26:44 -0400

    removing ui

--- react/password-validator/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PasswordInput from './PasswordInput';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-ReactDOM.render(<PasswordInput/>, document.getElementById('root'));
--- react/rating/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Rating from './Rating';
-
-ReactDOM.render(<Rating size='5' selectedRating='0'/>, document.getElementById('root'));
