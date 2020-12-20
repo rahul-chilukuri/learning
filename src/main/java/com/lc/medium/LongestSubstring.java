a2e5a46e8f3aa2e357f299088d97e5fbbddf0dd1
Rahul Chilukuri <rchilukuri@vscode.com>
2020-12-20 09:45:02 -0500

    LongestSubString

--- src/main/java/com/lc/medium/LongestSubstring.java
+++ src/main/java/com/lc/medium/LongestSubstring.java
@@ -7,6 +7,7 @@ import static com.sr.utils.InputUtil.sysout;
 public class LongestSubstring {
     public static void main(String[] args) {
         long s = System.currentTimeMillis();
+        long fm = Runtime.getRuntime().freeMemory();
         sysout(substrA("dvdf")); // vdf -> 3
         sysout(substrA("abcabcbadbcb")); // cbad -> 4
         sysout(substrA("abcabcbb")); // abc -> 3
@@ -18,25 +19,25 @@ public class LongestSubstring {
         sysout(substrA("bbbbb")); // b -> 1
         sysout(substrA(" "));
         sysout(substrA(""));
+        sysout((fm - Runtime.getRuntime().freeMemory()) / (1024));
         sysout(System.currentTimeMillis() - s);
         sysexit();
     }
 
     public static int substrA(String s) {
         int[] charset = new int[128];
-        Arrays.fill(charset, -1);
-        int c = 0, i = 0, count = 0, max = 0, len = s.length(), idx = 0;
+        int chr = 0, i = 0, pos = 0, count = 0, max = 0, len = s.length();
 
         while (i < len) {
-            c = s.charAt(i);
-            idx = charset[c];
-            if (idx > -1) {
+            chr = s.charAt(i);
+            pos = charset[chr];
+            if (pos > 0) {
                 max = Math.max(max, count);
-                Arrays.fill(charset, -1);
-                i = idx + 1;
+                Arrays.fill(charset, 0);
+                i = pos;
                 count = 0;
             } else {
-                charset[c] = i++;
+                charset[chr] = ++i;
                 count++;
             }
         }
@@ -45,7 +46,20 @@ public class LongestSubstring {
         return Math.max(count, max);
     }
 
-    static public int substrB(String s) {
+    // similar but shorted implementation to substrA
+    public static int substrB(String s) {
+        int n = s.length(), ans = 0;
+        int[] index = new int[128]; // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
+        }
+        return ans;
+    }
+
+    static public int substrC(String s) {
         int[] count = new int[128];
         int start = 0, end = 0, max = 0, cur = 0;
         while (end < s.length()) {
@@ -61,18 +75,5 @@ public class LongestSubstring {
             end++;
         }
         return Math.max(max, cur);
-    }
-
-    // similar but shorted implementation to substrA
-    public static int substrC(String s) {
-        int n = s.length(), ans = 0;
-        int[] index = new int[128]; // current index of character
-        // try to extend the range [i, j]
-        for (int j = 0, i = 0; j < n; j++) {
-            i = Math.max(index[s.charAt(j)], i);
-            ans = Math.max(ans, j - i + 1);
-            index[s.charAt(j)] = j + 1;
-        }
-        return ans;
     }
 }
